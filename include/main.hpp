0896060a000498e0a30290333e9571f9dfce56c4
Yubo Wang <yubo0809@outlook.com>
2024-05-23 21:16:47 -0400

    added more libs

---
+++ include/main.hpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+
+#include <Wire.h>
+#include <SPI.h>
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP3XX.h>
+#include <Adafruit_LSM6DS3TRC.h>
+#include <RH_RF95.h>
+#include <TinyGPS++.h>
+
+//Definitions
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+
+// Variables
+Adafruit_BMP3XX bmp; // barometer
+Adafruit_LSM6DS3TRC lsm; // IMU
+Adafruit_Sensor *lsm_accel, *lsm_gyro;
+sensors_event_t accelMain, gyro;
+// GPS
+RH_RF95 rf95; // Radio
+// SD Card
+// Flash
+
+// put function declarations here:
+
+/**
+ * @brief Initializes the BMP sensor on an I2C bus
+ * 
+ * @param I2CBus The I2C bus to use. Either Wire, Wire1, Wire2
+ */
+void initBMP(TwoWire* I2CBus);
+
+/**
+ * @brief Initializes the IMU sensor on an I2C bus
+ * 
+ * @param I2CBus The I2C bus to use. Either Wire, Wire1, Wire2
+ */
+void initIMU(TwoWire* I2CBus);
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,56 @@
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "main.hpp"
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  initBMP(BMP390_CHIP_ID, &Wire);
+  initIMU(106U, &Wire);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  double altitude = bmp.readAltitude(1013.25);
+  
+  lsm_accel->getEvent(&accelMain);
+  lsm_gyro->getEvent(&gyro);
+  
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void initBMP(uint8_t i2cAddr, TwoWire* I2CBus){
+  if (!bmp.begin_I2C(i2cAddr, &Wire)) {
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1);
+  }
+  Serial.println("BMP3 sensor found");
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_16X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_200_HZ);
+}
+
+void initIMU(uint8_t i2cAddr, TwoWire* I2CBus) {
+  if (!lsm.begin_I2C(i2cAddr, I2CBus)) {
+    // if (!lsm.begin_SPI(LSM_CS)) {
+    // if (!lsm.begin_SPI(LSM_CS, LSM_SCK, LSM_MISO, LSM_MOSI)) {
+    Serial.println("Failed to find LSM6DS chip");
+    while (1) {
+      delay(10);
+    }
+  }
+
+  Serial.println("LSM6DS Found!");
+
+  // Set to 2G range and 26 Hz update rate
+  lsm.setAccelRange(LSM6DS_ACCEL_RANGE_16_G);
+  lsm.setGyroRange(LSM6DS_GYRO_RANGE_2000_DPS);
+  lsm.setAccelDataRate(LSM6DS_RATE_833_HZ);
+  lsm.setGyroDataRate(LSM6DS_RATE_833_HZ);
+
+  lsm_accel = lsm.getAccelerometerSensor();
+  lsm_accel->printSensorDetails();
+
+  lsm_gyro = lsm.getGyroSensor();
+  lsm_gyro->printSensorDetails();
 }
