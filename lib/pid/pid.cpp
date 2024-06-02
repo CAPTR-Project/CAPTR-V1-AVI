a2c8133d619018325b9dfb4981979b758092b4f0
Tommy Zhang <tommyz2005@gmail.com>
2024-06-01 22:53:33 -0400

    Refactor PID class (transfer to ubuntu)

--- lib/pid/pid.cpp
+++ lib/pid/pid.cpp
@@ -1,11 +1,69 @@
 #include "pid.hpp"
 
 // pid class
-
+using namespace std;
 class PID
 {
-public:
+    public:
+        initPID(double dt, double max, double min, double Kp, double Kd, double Ki);
+        ~PID();
+        double update(double input);
 
-private:
+    private:
+        double _Kp, _Ki, _Kd;
+        double _min, _max;
+        double _dt;
+        double _setpoint;
+        double _integral;
+        double _prevError;
+};
 
+PID::initPID(double dt, double max, double min, double Kp, double Kd, double Ki) :
+{
+    _Kp(Kp);
+    _Kd(Kd);
+    _Ki(Ki);
+    _max(max);
+    _min(min);
+    _dt(dt);
+    return this;
+}
+
+
+void setDt(float dt)
+{
+    this->dt = dt;
+}
+
+void setSetpoint(float setpoint)
+{
+    this->setpoint = setpoint;
+}
+
+double update(double input)       // main PID loop
+{
+
+    float error = setpoint - input;                                 // proportional
+    integral += error * dt;                                         // integral 
+    float derivative = (error - prevError) / dt;                    // derivative
+
+    prevError = error;                                              
+    float output = kp * error + ki * integral + kd * derivative;
+
+    // clamping to min max outputs (i assume this will be vectoring angles)
+    if (output < min)
+    {
+        output = min;
+    }
+    else if (output > max)
+    {
+        output = max;
+    }
+    return output;
+}
+
+void reset()    // reset accumulated error and values for new setpoint 
+{
+    integral = 0.0;
+    prevError = 0.0;
 }
--- lib/pid/pid.hpp
+++ lib/pid/pid.hpp
@@ -1,3 +1,24 @@
 #include <stdio.h>
 
+// pid controller
+
+class PID
+{
+    public:
+        PID();
+        ~PID();
+        void setGains(float kp, float ki, float kd);
+        void setLimits(float min, float max);
+        void setDt(float dt);
+        void setSetpoint(float setpoint);
+        float update(float input);
+        void reset();
+    private:
+        float kp, ki, kd;
+        float min, max;
+        float dt;
+        float setpoint;
+        float integral;
+        float prevError;
+};
 
